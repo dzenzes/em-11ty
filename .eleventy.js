1502851de7eaf993150d497e91aa0a04b5ecdde7
Daniel Mies <daniel.mies@codecentric.de>
2021-03-17 21:29:01 +0100

    added some filters

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,17 @@
-module.exports = function () {
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addFilter("toIsoString", function (date) {
+    return date.toISOString();
+  });
+
+  eleventyConfig.addFilter("toReadableDate", function (date) {
+    let month = "" + (date.getMonth() + 1);
+    let day = "" + date.getDate();
+    let year = date.getFullYear();
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
+
+    return `${day}.${month}.${year}`;
+  });
   return {
     dir: {
       input: "src",
